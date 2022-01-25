5e0805d8cf0337d08b4d91a2aec7d1a5be464ef1
StepanTchynetskyiDS <stepan.tchynetskyi@deepsee.ai>
2022-01-25 12:47:14 +0200

    declared db, ma, migrations

--- application/__init__.py
+++ application/__init__.py
@@ -1,14 +1,19 @@
 import os
 from flask import Flask
+from application.db import db, migrate
+from application.ma import ma
 
 
-def create_app(test_config=None):
-    """Factory to create the Flask application
-    :return: A `Flask` application instance
-    """
+def create_app():
     app = Flask(__name__)
     app.config.from_object(os.environ['APP_SETTINGS'])
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     app.config["PROPAGATE_EXCEPTIONS"] = True
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    @app.before_first_request
+    def create_tables():
+        db.create_all()
 
     return app
